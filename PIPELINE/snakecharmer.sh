3636934583f2919b4dbcb8476883598f8028fde4
Ryan Sheridan <ryan.sheridan@ucdenver.edu>
2020-02-14 15:50:47 -0700

    added new pipeline

--- PIPELINE/snakecharmer.sh
+++ PIPELINE/snakecharmer.sh
@@ -1,21 +1,37 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
-snakemake="$HOME/.local/bin/snakemake"
-res_dir="$HOME/RESULTS"
-log_dir="$res_dir/logs"
-pipeline="$HOME/PIPELINE"
-threads=$(grep -c "^processor" /proc/cpuinfo)
-threads=$(expr "$threads" - 2)
+#BSUB -J cellranger
+#BSUB -o logs/cellranger_%J.out
+#BSUB -e logs/cellranger_%J.err
+#BSUB -R "select[mem>4] rusage[mem=4]" 
+#BSUB -q rna
 
-mkdir -p "$log_dir"
+set -o nounset -o pipefail -o errexit -x
+
+mkdir -p logs
 
 
-"$snakemake" \
-    --snakefile "$pipeline/Snakefile" \
-    --configfile "$HOME/config.yaml" \
-    --cores "$threads" \
-    --latency-wait 60 \
-    --rerun-incomplete \
-    > "$log_dir/cellranger.out" \
-    2>&1
+# Function to run snakemake
+run_snakemake() {
+    local num_jobs=$1
+    local config_file=$2
+    
+    drmaa_args='
+        -o {log}.out 
+        -e {log}.err 
+        -J {params.job_name} 
+        -R "{params.memory} span[hosts=1] " 
+        -n {threads} '
+
+    snakemake \
+        --snakefile Snakefile \
+        --drmaa "$drmaa_args" \
+        --jobs $num_jobs \
+        --latency-wait 60 \
+        --rerun-incomplete \
+        --configfile $config_file
+}
+
+run_snakemake 50 config.yaml
+
 
