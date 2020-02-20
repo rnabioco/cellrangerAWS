8bdf5a1f81f35364c2b413a940ea6246e4b7e9d1
rmsheridan <ryan.sheridan@ucdenver.edu>
2020-02-20 12:38:27 -0700

    fixed sed error in snakecharmer, updated AMI

--- PIPELINE/snakecharmer.sh
+++ PIPELINE/snakecharmer.sh
@@ -6,15 +6,37 @@ log_dir="$res_dir/logs"
 pipeline="$HOME/PIPELINE"
 threads=$(grep -c "^processor" /proc/cpuinfo)
 threads=$(expr "$threads" - 2)
+S3="BUCKET"
+EC2="INSTANCE"
 
 mkdir -p "$log_dir"
 
+get_time() {
+    echo "["$(date "+%F")"]"
+}
 
+
+# Run Cell Ranger
 "$snakemake" \
     --snakefile "$pipeline/Snakefile" \
     --configfile "$pipeline/config.yaml" \
     --cores "$threads" \
     --latency-wait 60 \
-    > "$log_dir/cellranger.out" \
-    2>&1
+    &> "$log_dir/cellranger.out"
+
+
+# Transfer results and terminate instance
+echo -e "\n$(get_time) Cell Ranger run complete, check logs for any errors."
+
+echo -e "\n$(get_time) Transferring results to $S3..."
+
+aws s3 cp --recursive "$HOME/RESULTS" "$S3/RESULTS"
+aws s3 cp --recursive "$HOME/PIPELINE" "$S3/PIPELINE"
+
+echo -e "\n$(get_time) Terminating EC2 instance $EC2..."
+
+aws ec2 terminate-instances \
+    --instance-ids "$EC2" \
+    > /dev/null
+
 
