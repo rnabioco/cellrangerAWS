4a5dcd8c8ace188c7cc72d2480b7b40e7e0f5d7a
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2020-02-22 17:01:47 -0700

    added nohup so ssh session can be disconnected

--- PIPELINE/snakecharmer.sh
+++ PIPELINE/snakecharmer.sh
@@ -16,18 +16,20 @@ get_time() {
     echo "["$(date "+%F")"]"
 }
 
-# Run Cell Ranger, transfer results, terminate instance
+# Run Cell Ranger
 "$snakemake" \
     --snakefile "$pipeline/Snakefile" \
     --configfile "$pipeline/$YAML" \
     --cores "$threads" \
     --latency-wait 60 \
-    &> "$log_dir/cellranger.out" \
-    && echo -e "\n$(get_time) Cell Ranger run complete, check logs for any errors." \
-    && echo -e "\n$(get_time) Transferring results to $S3..." \
-    && aws s3 cp --recursive "$HOME/RESULTS" "$S3/RESULTS" \
-    && aws s3 cp --recursive "$HOME/PIPELINE" "$S3/PIPELINE" \
-    && echo -e "\n$(get_time) Terminating EC2 instance $EC2..." \
-    && aws ec2 terminate-instances --instance-ids "$EC2" > /dev/null
+    &> "$log_dir/cellranger.out"
+
+# Transfer results and terminate instance
+aws s3 cp --recursive "$HOME/RESULTS" "$S3/RESULTS"
+aws s3 cp --recursive "$HOME/PIPELINE" "$S3/PIPELINE"
+
+aws ec2 terminate-instances \
+    --instance-ids "$EC2" \
+    > /dev/null
 
 
